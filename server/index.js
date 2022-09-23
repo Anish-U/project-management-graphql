9b85f91bde0a81c346504d06e46d9d2c78473a8f
Ummenthala Anish <ummenthala.anish@gmail.com>
2022-09-23 20:19:34 +0530

    Bugs Fixed in Server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const { graphqlHTTP } = require('express-graphql');
 const colors = require('colors');
+const cors = require('cors');
 
 const schema = require('./schema/schema');
 const connectDB = require('./config/db');
@@ -12,6 +13,8 @@ connectDB();
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(cors());
 
 app.use(
 	'/graphql',
