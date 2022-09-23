8b6ff186f847d879a70327b16742571b72db4c5c
Ummenthala Anish <ummenthala.anish@gmail.com>
2022-09-23 20:19:51 +0530

    FrontEnd Completed

---
+++ client/src/components/Projects.jsx
@@ -0,0 +1,30 @@
+import { useQuery } from '@apollo/client';
+
+import { GET_PROJECTS } from '../queries/projectQueries';
+
+import Spinner from './Spinner';
+import ProjectCard from './ProjectCard';
+
+const Projects = () => {
+	const { loading, error, data } = useQuery(GET_PROJECTS);
+
+	if (loading) return <Spinner />;
+
+	if (error) return <p>{console.log(error)}</p>;
+
+	return (
+		<>
+			{data.projects.length > 0 ? (
+				<div className='row'>
+					{data.projects.map((project) => (
+						<ProjectCard key={project.id} project={project} />
+					))}
+				</div>
+			) : (
+				<p>No Projects</p>
+			)}
+		</>
+	);
+};
+
+export default Projects;
